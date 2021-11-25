c9babff2e97fdf103f64a228319fdf8a24fc4244
CowKeyMan <dancauchi1@gmail.com>
2021-11-25 20:23:00 +0200

    Add specific train and test set for consistency across different personnel

---
+++ data_wrangling/generate_train_test.py
@@ -0,0 +1,14 @@
+import sys
+sys.path += ['machine_learning']
+
+import numpy as np
+import pandas as pd
+from utils import get_stratified_train_test_folds
+np.random.seed(42)
+
+
+df = pd.read_csv('resources/data/original/npf_train.csv')
+train_df, test_df = \
+    get_stratified_train_test_folds(df, 'class4', test_factor=0.2)
+train_df.to_csv('resources/data/generated/train_train.csv')
+test_df.to_csv('resources/data/generated/train_test.csv')
